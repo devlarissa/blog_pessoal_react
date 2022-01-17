a6ade2df201c971fcc800053230e36536f41d5ca
devlarissa <devlarissasilva@gmail.com>
2022-01-17 19:31:27 -0300

    Atualizando repositorio

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import {Grid} from '@material-ui/core'
+import Home from './paginas/home/Home'
+import './App.css';
+
+
+let nome = "Larissa";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
